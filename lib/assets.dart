351e7af8254006fa0def2784f5924475c0553189
Rasoul-Hassan <rasulprogramming092@gmail.com>
2025-09-19 08:03:16 +0300

    add rive icon and animated drawer and bottom navbar

---
+++ lib/assets.dart
@@ -0,0 +1,7 @@
+// Rive assets
+const String iconsRiv = 'assets/icons/icons.riv';
+const String shapesRiv = 'assets/icons/shapes.riv';
+const String buttonRiv = 'assets/icons/button.riv';
+const String checkRiv = 'assets/icons/check.riv';
+const String confettiRiv = 'assets/icons/confetti.riv';
+const String menuButtonRiv = 'assets/icons/menu_button.riv';
